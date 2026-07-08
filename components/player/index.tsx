85e90d6d33fdc66a99e36b80be8b1fa573865b9a
Aniket Saini <aniketxdop@gmail.com>
2026-07-09 00:56:50 +0530

    Reorder: CinemaOS 1, Cinezo 2, ScreenScape 3

--- components/player/index.tsx
+++ components/player/index.tsx
@@ -20,12 +20,12 @@ interface MyPlayerProps {
 }
 
 const ALL_SERVERS = [
-  // 1 - player.cinezo.live
-  { name: "Cinezo", url: (id: string, isTv: boolean, s = 1, e = 1) => isTv ? `https://player.cinezo.live/embed/tv/${id}/${s}/${e}?autoplay=true&muted=0` : `https://player.cinezo.live/embed/movie/${id}?autoplay=true&muted=0` },
-  // 2 - screenscape.me
-  { name: "ScreenScape", url: (id: string, isTv: boolean, s = 1, e = 1) => isTv ? `https://screenscape.me/embed?tmdb=${id}&type=tv&s=${s}&e=${e}&autoplay=1&muted=0` : `https://screenscape.me/embed?tmdb=${id}&type=movie&autoplay=1&muted=0` },
-  // 3 - cinemaos.tech
+  // 1 - cinemaos.tech
   { name: "CinemaOS", url: (id: string, isTv: boolean, s = 1, e = 1) => isTv ? `https://cinemaos.tech/player/${id}/${s}/${e}?autoplay=1&muted=0` : `https://cinemaos.tech/player/${id}?autoplay=1&muted=0` },
+  // 2 - player.cinezo.live
+  { name: "Cinezo", url: (id: string, isTv: boolean, s = 1, e = 1) => isTv ? `https://player.cinezo.live/embed/tv/${id}/${s}/${e}?autoplay=true&muted=0` : `https://player.cinezo.live/embed/movie/${id}?autoplay=true&muted=0` },
+  // 3 - screenscape.me
+  { name: "ScreenScape", url: (id: string, isTv: boolean, s = 1, e = 1) => isTv ? `https://screenscape.me/embed?tmdb=${id}&type=tv&s=${s}&e=${e}&autoplay=1&muted=0` : `https://screenscape.me/embed?tmdb=${id}&type=movie&autoplay=1&muted=0` },
   // 4 - rozgarlelo.modiplay.xyz
   { name: "razor[Hindi]", url: (id: string, isTv: boolean, s = 1, e = 1) => isTv ? `https://rozgarlelo.modiplay.xyz/embed/tmdb/tv?id=${id}&s=${s}&e=${e}&autoplay=1&muted=0` : `https://rozgarlelo.modiplay.xyz/embed/tmdb/movie?id=${id}&autoplay=1&muted=0` },
   // 5 - embed.filmu.in
