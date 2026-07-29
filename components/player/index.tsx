11649517cba239ab1b25b696bec238297757d6c6
Aniket Saini <aniketxdop@gmail.com>
2026-07-29 22:39:12 +0530

    Swap server 2 and 3 and fix embed links

--- components/player/index.tsx
+++ components/player/index.tsx
@@ -22,7 +22,9 @@ interface MyPlayerProps {
 const ALL_SERVERS = [
   // 1 - anixhub-stream.vercel.app
   { name: "AnixHub", url: (id: string, isTv: boolean, s = 1, e = 1) => isTv ? `https://anixhub-stream.vercel.app/?tmdb_id=${id}&media_type=tv&season=${s}&episode=${e}` : `https://anixhub-stream.vercel.app/?tmdb_id=${id}&media_type=movie` },
-  // 2 - streams.iqsmartgames.com
+  // 2 - rozgarlelo
+  { name: "RozgarLelo", url: (id: string, isTv: boolean, s = 1, e = 1) => isTv ? `https://rozgarlelo.modiplay.xyz/embed/tmdb/tv?id=${id}&s=${s}&e=${e}` : `https://rozgarlelo.modiplay.xyz/embed/tmdb/movie?id=${id}` },
+  // 3 - streams.iqsmartgames.com
   { name: "MultiMovies", url: (id: string, isTv: boolean, s = 1, e = 1) => isTv ? `https://streams.iqsmartgames.com/embed/tv/${id}/${s}/${e}?key=e11a7debaaa4f5d25b671706ffe4d2acb56efbd4` : `https://streams.iqsmartgames.com/embed/movie/${id}?key=e11a7debaaa4f5d25b671706ffe4d2acb56efbd4` },
 ];
 
